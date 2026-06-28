e882bfc9983cf2491384d92246933422fa3cde9f
EPatro <71399081+enpatro@users.noreply.github.com>
2026-06-29 01:40:23 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 // Paste real Firebase config here only. Keep variable name firebaseConfig.
 const firebaseConfig = {
-  apiKey: "PASTE_REAL_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyAG_dEacwKxSwHqQWxYwKpipI8P5tMZdo",
+  authDomain: "pemrm1-8b213.firebaseapp.com",
+  projectId: "pemrm1-8b213",
+  storageBucket: "pemrm1-8b213.firebasestorage.app",
+  messagingSenderId: "657822169153",
+  appId: "1:657822169153:web:5dd06f087f095f15202ec0"
+
 };
